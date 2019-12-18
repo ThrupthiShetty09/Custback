f0ad0892683cc2b2806c6c2e6fc2c4f4b6e74c9f
slk <slk@10.60.24.92>
2019-12-18 16:51:31 +0530

    jnbjn

--- src/main/java/com/slk/controller/CustomerRestController.java
+++ src/main/java/com/slk/controller/CustomerRestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.slk.DAO.CustomerDAO;
-import com.slk.model.Agent;
+
 import com.slk.model.Customer;
 
 
